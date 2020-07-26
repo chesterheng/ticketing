2353aa4f81fe2202bae156ba8378f25ea86624f9
Chester Heng <chester.heng@gmail.com>
2020-07-26 17:27:09 +0800

    Section 23: CI/CD

---
+++ payments/src/routes/new.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+} from '@chticketing/common';
+import { stripe } from '../stripe';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.post(
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
+    res.status(201).send({ success: true });
+  }
+);
+
+export { router as createChargeRouter };
